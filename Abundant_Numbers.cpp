9314a369100fa762bc8b0dd636d0d57589081322
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-02 21:31:13 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int sum=0;     for(int i=1;i<=n/2;i++)     {         if(n%i==0)         {             sum=sum+i;         }     }     if(sum>n)     {         cout<<"True";     }     else     {         cout<<"False";     }
    }

---
+++ Abundant_Numbers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum>n)
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+    
+}
