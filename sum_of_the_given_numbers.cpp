a2bddb15301b9a1093ea2a1c9a4b917500657a3c
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-02 19:16:19 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int t;     cin>>t;     int r;     while(t!=0)     {         int a,b;         cin>>a;         cin>>b;          r=a+b;          cout<<r<<" ";     t--;
        }
        
    }

---
+++ sum_of_the_given_numbers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    int r;
+    while(t!=0)
+    {
+        int a,b;
+        cin>>a;
+        cin>>b;
+         r=a+b;
+         cout<<r<<"
+";
+    t--;
+        
+    }
+    
+}
