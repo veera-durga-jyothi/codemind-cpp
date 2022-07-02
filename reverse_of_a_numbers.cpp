97454c88ba5802df251628537ad711c99c0f416a
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-02 19:30:25 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int sum=0;     while(n!=0)     {         int rem=n%10;         sum=sum*10+rem;         n=n/10;     }     cout<<sum; }

---
+++ reverse_of_a_numbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    while(n!=0)
+    {
+        int rem=n%10;
+        sum=sum*10+rem;
+        n=n/10;
+    }
+    cout<<sum;
+}
