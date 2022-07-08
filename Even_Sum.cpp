e983657d6bef86e0c072fc1de9939f450adbdc28
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-08 16:30:15 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++)     {         cin>>arr[i];     }     int o=0;     for(int i=0;i<n;i++)     {         if(arr[i]%2==0)         {             o=o+arr[i];         }     }     cout<<o; }

---
+++ Even_Sum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int o=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            o=o+arr[i];
+        }
+    }
+    cout<<o;
+}
