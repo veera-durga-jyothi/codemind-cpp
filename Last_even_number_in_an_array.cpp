c71f60ae6f3582c52d19294d722ee133158d877d
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-08 17:15:51 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++)     {         cin>>arr[i];     }     vector<int>r;
        for(int i=0;i<n;i++)
        {
            if(arr[i]%2==0)
            {
                r.push_back(arr[i]);
                
            }
            
        }
        cout<<r[r.size()-1];
    }

---
+++ Last_even_number_in_an_array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    vector<int>r;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            r.push_back(arr[i]);
+            
+        }
+        
+    }
+    cout<<r[r.size()-1];
+}
