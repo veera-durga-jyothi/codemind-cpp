82017866c43589411b2d68071c16ad3818d9f70f
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-09 14:34:52 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++)     {         cin>>arr[i];     }     int c=0;     for(int i=1;i<n-1;i++)     {         if(arr[i]%2!=0)         {             if(arr[i-1]%2==0 && arr[i+1]%2==0)             {                 c++;             }
            }
           
                
            
        }
        cout<<c;
    }

---
+++ count_odd_numbers_between_even_numbers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int c=0;
+    for(int i=1;i<n-1;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            if(arr[i-1]%2==0 && arr[i+1]%2==0)
+            {
+                c++;
+            }
+            
+        }
+       
+            
+        
+    }
+    cout<<c;
+}
