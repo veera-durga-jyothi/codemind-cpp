52bcacd82a1a0a839982328577350fe85efe5212
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-10 18:26:41 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++)     {         cin>>arr[i];     }     int a,b;     cin>>a>>b;     int t=0,f=0,sum=0;     for(int i=0;i<n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             t++;
            }
            else
            {
                f++;
                sum=sum+arr[i];
            }
        }
        
        
            cout<<sum;
        
    }

---
+++ sum_of_elements_not_between_A_and_B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int a,b;
+    cin>>a>>b;
+    int t=0,f=0,sum=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            t++;
+            
+        }
+        else
+        {
+            f++;
+            sum=sum+arr[i];
+        }
+    }
+    
+    
+        cout<<sum;
+    
+}
