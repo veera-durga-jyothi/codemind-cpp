aebf50c14def04fcd304339b73bf603fb3073472
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-07-12 14:09:34 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n;     cin>>n;     int arr[n];     for(int i=0;i<n;i++)     {         cin>>arr[i];     }     int f=0;     for(int i=0;i<n;i++)     {         if(arr[i]%2==0 && i%2!=0)         {             f++;             break;         }     }     if(f==0)     {         cout<<"True";     }     else     {         cout<<"False";     } }

---
+++ Strictly_EVEN.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int f=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2==0 && i%2!=0)
+        {
+            f++;
+            break;
+        }
+    }
+    if(f==0)
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+}
